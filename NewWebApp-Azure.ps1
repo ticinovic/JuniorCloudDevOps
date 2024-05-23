14e7252d460e147ce2b0e3079321e4bc874d8b45
MarioT <147745709+ticinovic@users.noreply.github.com>
2024-05-23 14:29:25 +0200

    NewWebApp-Azure.ps1

--- NewWebApp-Azure.ps1
+++ NewWebApp-Azure.ps1
@@ -6,7 +6,7 @@ $subscriptionName = Read-Host "Enter the subscription name"
 $resourceGroupName = Read-Host "Enter the resource group name"
 $appServicePlanName = Read-Host "Enter the app service plan name"
 $newWebAppName = Read-Host "Enter the new web app name"
-$location = Read-Host "Enter the location (e.g., West Europe)"
+$location = Read-Host "Enter the location"
 
 # Set the subscription
 Set-AzContext -Subscription $subscriptionName
@@ -16,5 +16,3 @@ New-AzWebApp -ResourceGroupName $resourceGroupName `
              -Name $newWebAppName `
              -Location $location `
              -AppServicePlan $appServicePlanName
-
-Write-Output "Web App $newWebAppName created successfully."
