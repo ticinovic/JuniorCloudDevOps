c30ec925c95716850fdb16df5537519482a0b6bc
MarioT <147745709+ticinovic@users.noreply.github.com>
2024-05-23 11:21:12 +0200

    NewWebApp

---
+++ NewWebApp-Azure.ps1
@@ -0,0 +1,14 @@
+﻿Connect-AzAccount
+
+$RG = "JuniorCloudDevOps"
+$ASP = "AppServicePlan"
+$newWebApp = "myWebApp5"
+$location = "West Europe"
+
+# Create the Web App
+New-AzWebApp -ResourceGroupName $RG `
+             -Name $newWebApp `
+             -Location $location `
+             -AppServicePlan $ASP
+
+Write-Output "Web App $newWebAppName created successfully."
